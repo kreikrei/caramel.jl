852f5ee6c027308136861b9d3e3f3b81db0e0e2b
kreikrei <kreitonstrs@gmail.com>
2021-08-04 20:53:55 +0700

    add basic struct: satker, moda, lin

---
+++ src/struct.jl
@@ -0,0 +1,44 @@
+"""
+    `satker` is "satuan kerja" and contains its position to compute distances, its max and min capacity, and inventory flows at the begining and throughout the planning horizon.
+"""
+struct satker
+    #coordinates
+    x::Float64
+    y::Float64
+
+    #capacities
+    MAX::Int64
+    MIN::Int64
+
+    #inventory flows
+    START::Int64
+    d::Vector{Int64}
+end
+
+"""
+    moda is the type of transportation. this will be called by lins. The struct contains building blocks for cost functions and load-trip multiples.
+"""
+struct moda
+    #capacity
+    Q::Int64
+
+    #fixed cost
+    con::Float64
+    dis::Float64
+
+    #variable cost
+    var::Float64
+end
+
+"""
+    lin is the edge of the multigraph it shows the triplet source, destination, and mode of an edge and it also contains the availability of that segment.
+"""
+struct lin
+    #triplet (i,j,k)
+    src::String
+    dst::String
+    md::String
+
+    #limit of use
+    w::Int64
+end
